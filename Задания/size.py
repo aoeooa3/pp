1a73b8fce4751cfa8eec426cbd34d8309a4f62b6
aoeooa3 <evacistakova31892@gmail.com>
2024-11-20 23:15:27 +0300

    Delete Задания/size.py

--- Задания/size.py
+++
@@ -1,8 +0,0 @@
-from main import image1_path, image2_path
-
-def resize_image(image_path, new_size):
-    img = Image.open(image_path)
-    img_size = img.resize((250, 90)) 
-
-    img1_size = img1.resize((250, 90)) 
-
